d924d60a9f4aa6d3ca28a79e5bd044f689ee3a03
sdongary <siddharth.sachin89@gmail.com>
2021-10-09 23:36:11 -0500

    4th commit

--- src/main/java/variousConcepts/GitTest2.java
+++ src/main/java/variousConcepts/GitTest2.java
@@ -5,7 +5,13 @@ public class GitTest2 {
 	public static void main(String[] args) {
 		System.out.println("Git Test");
 		System.out.println("Git Test1");
-		System.out.println("Git Test2");
+		printMe();
 	}
-
+		public static void printMe() {
+			System.out.println("Git Test2");
+			System.out.println("Git Test3");
+			System.out.println("Git Test4");
+		}
+	
+	
 }
